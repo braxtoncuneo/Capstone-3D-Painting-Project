90cbe24eda1166b85c09335c8490f92cc62430c0
Sanlador <cunardr@oregonstate.edu>
2018-02-15 19:47:47 -0800

    Voxel Alpha
    Completed alpha build of voxel engine. Replaced pre-build code with my
    own and stripped away unnecessary elements. Voxel engine is now capable
    of creating, destroying, and modifying voxels.

---
+++ RichardCunard/Voxel Alpha/Assets/Scripts/Chunk.cs
@@ -0,0 +1,329 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    private List<Vector3> tempVerts = new List<Vector3>();
+    private List<int> tempTri = new List<int>();
+    private List<Vector2> tempUV = new List<Vector2>();
+
+    private float textureOffset = 0.25f;
+    private Vector2 blue = new Vector2(2.0f, 0.0f);
+    private Vector2 green = new Vector2(0.0f, 2.0f);
+    private Vector2 red = new Vector2(0.0f, 0.0f);
+    private Vector2 yellow = new Vector2(2.0f, 2.0f);
+
+    private Mesh mesh;
+    private World world;
+    private int polyCount;
+
+    public int chunkSize = 100;
+    public GameObject worldReference;
+
+    public int chunkX;
+    public int chunkY;
+    public int chunkZ;
+
+
+    public bool update;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        world = worldReference.GetComponent("World") as World;
+        GenerateMesh();
+    }
+
+    void LateUpdate()
+    {
+        if (update)
+        {
+            GenerateMesh();
+            update = false;
+        }
+    }
+
+    void CubeTop(int x, int y, int z, int block)
+    {
+        tempVerts.Add(new Vector3(x, y, z + 1));
+        tempVerts.Add(new Vector3(x + 1, y, z + 1));
+        tempVerts.Add(new Vector3(x + 1, y, z));
+        tempVerts.Add(new Vector3(x, y, z));
+
+        Vector2 texturePos = new Vector2(0, 0);
+
+        if (Block(x, y, z) == 1)
+        {
+            texturePos = blue;
+        }
+        else if (Block(x, y, z) == 2)
+        {
+            texturePos = green;
+        }
+        else if (Block(x, y, z) == 3)
+        {
+            texturePos = red;
+        }
+        else if (Block(x, y, z) == 4)
+        {
+            texturePos = yellow;
+        }
+
+        Cube(texturePos);
+    }
+
+    void CubeFront(int x, int y, int z, int block)
+    {
+        tempVerts.Add(new Vector3(x + 1, y - 1, z + 1));
+        tempVerts.Add(new Vector3(x + 1, y, z + 1));
+        tempVerts.Add(new Vector3(x, y, z + 1));
+        tempVerts.Add(new Vector3(x, y - 1, z + 1));
+
+        Vector2 texturePos = new Vector2(0, 0);
+
+        if (Block(x, y, z) == 1)
+        {
+            texturePos = blue;
+        }
+        else if (Block(x, y, z) == 2)
+        {
+            texturePos = green;
+        }
+        else if (Block(x, y, z) == 3)
+        {
+            texturePos = red;
+        }
+        else if (Block(x, y, z) == 4)
+        {
+            texturePos = yellow;
+        }
+
+        Cube(texturePos);
+
+    }
+
+    void CubeRight(int x, int y, int z, int block)
+    {
+        tempVerts.Add(new Vector3(x + 1, y - 1, z));
+        tempVerts.Add(new Vector3(x + 1, y, z));
+        tempVerts.Add(new Vector3(x + 1, y, z + 1));
+        tempVerts.Add(new Vector3(x + 1, y - 1, z + 1));
+
+        Vector2 texturePos = new Vector2(0, 0);
+
+        if (Block(x, y, z) == 1)
+        {
+            texturePos = blue;
+        }
+        else if (Block(x, y, z) == 2)
+        {
+            texturePos = green;
+        }
+        else if (Block(x, y, z) == 3)
+        {
+            texturePos = red;
+        }
+        else if (Block(x, y, z) == 4)
+        {
+            texturePos = yellow;
+        }
+
+        Cube(texturePos);
+
+    }
+
+    void CubeBack(int x, int y, int z, int block)
+    {
+        tempVerts.Add(new Vector3(x, y - 1, z));
+        tempVerts.Add(new Vector3(x, y, z));
+        tempVerts.Add(new Vector3(x + 1, y, z));
+        tempVerts.Add(new Vector3(x + 1, y - 1, z));
+
+        Vector2 texturePos = new Vector2(0, 0);
+
+        if (Block(x, y, z) == 1)
+        {
+            texturePos = blue;
+        }
+        else if (Block(x, y, z) == 2)
+        {
+            texturePos = green;
+        }
+        else if (Block(x, y, z) == 3)
+        {
+            texturePos = red;
+        }
+        else if (Block(x, y, z) == 4)
+        {
+            texturePos = yellow;
+        }
+
+        Cube(texturePos);
+
+    }
+
+    void CubeLeft(int x, int y, int z, int block)
+    {
+        tempVerts.Add(new Vector3(x, y - 1, z + 1));
+        tempVerts.Add(new Vector3(x, y, z + 1));
+        tempVerts.Add(new Vector3(x, y, z));
+        tempVerts.Add(new Vector3(x, y - 1, z));
+
+        Vector2 texturePos = new Vector2(0, 0);
+
+        if (Block(x, y, z) == 1)
+        {
+            texturePos = blue;
+        }
+        else if (Block(x, y, z) == 2)
+        {
+            texturePos = green;
+        }
+        else if (Block(x, y, z) == 3)
+        {
+            texturePos = red;
+        }
+        else if (Block(x, y, z) == 4)
+        {
+            texturePos = yellow;
+        }
+
+        Cube(texturePos);
+
+    }
+
+    void CubeBot(int x, int y, int z, int block)
+    {
+        tempVerts.Add(new Vector3(x, y - 1, z));
+        tempVerts.Add(new Vector3(x + 1, y - 1, z));
+        tempVerts.Add(new Vector3(x + 1, y - 1, z + 1));
+        tempVerts.Add(new Vector3(x, y - 1, z + 1));
+
+        Vector2 texturePos = new Vector2(0, 0);
+
+        if (Block(x, y, z) == 1)
+        {
+            texturePos = blue;
+        }
+        else if (Block(x, y, z) == 2)
+        {
+            texturePos = green;
+        }
+        else if (Block(x, y, z) == 2)
+        {
+            texturePos = red;
+        }
+        else if (Block(x, y, z) == 2)
+        {
+            texturePos = yellow;
+        }
+
+        Cube(texturePos);
+
+    }
+
+    void Cube(Vector2 texturePos)
+    {
+        tempTri.Add(polyCount * 4); 
+        tempTri.Add(polyCount * 4 + 1); 
+        tempTri.Add(polyCount * 4 + 2); 
+        tempTri.Add(polyCount * 4); 
+        tempTri.Add(polyCount * 4 + 2); 
+        tempTri.Add(polyCount * 4 + 3); 
+
+        for (int i = 0; i < 4; i++)
+        tempUV.Add(new Vector2(textureOffset * texturePos.x + textureOffset, textureOffset * texturePos.y));
+        tempUV.Add(new Vector2(textureOffset * texturePos.x + textureOffset, textureOffset * texturePos.y + textureOffset));
+        tempUV.Add(new Vector2(textureOffset * texturePos.x, textureOffset * texturePos.y + textureOffset));
+        tempUV.Add(new Vector2(textureOffset * texturePos.x, textureOffset * texturePos.y));
+
+        polyCount++; // Add this line
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = tempVerts.ToArray();
+        mesh.uv = tempUV.ToArray();
+        mesh.triangles = tempTri.ToArray();
+        mesh.RecalculateNormals();
+
+        tempVerts.Clear();
+        tempUV.Clear();
+        tempTri.Clear();
+
+        polyCount = 0; //Fixed: Added this thanks to a bug pointed out by ratnushock!
+    }
+
+    //Renders any voxel side that is visible
+    public void GenerateMesh()
+    {
+
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    //This code will run for every block in the chunk
+
+                    if (Block(x, y, z) != 0)
+                    {
+                        //If the block is solid
+
+                        if (Block(x, y + 1, z) == 0)
+                        {
+                            //Block above is air
+                            CubeTop(x, y, z, 0);
+                        }
+
+                        if (Block(x, y - 1, z) == 0)
+                        {
+                            //Block below is air
+                            CubeBot(x, y, z, Block(x, y, z));
+
+                        }
+
+                        if (Block(x + 1, y, z) == 0)
+                        {
+                            //Block east is air
+                            CubeRight(x, y, z, Block(x, y, z));
+
+                        }
+
+                        if (Block(x - 1, y, z) == 0)
+                        {
+                            //Block west is air
+                            CubeLeft(x, y, z, Block(x, y, z));
+
+                        }
+
+                        if (Block(x, y, z + 1) == 0)
+                        {
+                            //Block north is air
+                            CubeFront(x, y, z, Block(x, y, z));
+
+                        }
+
+                        if (Block(x, y, z - 1) == 0)
+                        {
+                            //Block south is air
+                            CubeBack(x, y, z, Block(x, y, z));
+
+                        }
+
+                    }
+
+                }
+            }
+        }
+
+        UpdateMesh();
+    }
+
+    int Block(int x, int y, int z)
+    {
+        return world.Block(x + chunkX, y + chunkY, z + chunkZ);
+    }
+}
---
+++ RichardCunard/Voxel Alpha/Assets/Scripts/World.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class World : MonoBehaviour
+{
+    public int[,,] vState;
+    public int worldX = 16;
+    public int worldY = 16;
+    public int worldZ = 16;
+
+    public GameObject chunk;
+    public Chunk[,,] chunks;
+    public int chunkSize = 16;
+
+    void Start ()
+    {
+        //initialize a byte array using the predefined world size
+        vState = new int[worldX, worldY, worldZ];
+
+        //fill world with blocks
+        for (int x = 0; x < worldX; x++)
+        {
+            //Generate a 'starting canvas' for testing purposes
+            for (int y = 0; y < worldY; y++)
+            {
+                for (int z = 0; z < worldZ; z++)
+                {
+                    if (0 == y)
+                        vState[x, y, z] = 1;
+                    else
+                        vState[x, y, z] = 0;
+                }
+            }
+        }
+
+        chunks = new Chunk[Mathf.FloorToInt(worldX / chunkSize),
+        Mathf.FloorToInt(worldY / chunkSize),
+        Mathf.FloorToInt(worldZ / chunkSize)];
+    }
+
+    public bool checkCoords(int x, int y, int z)
+    {
+        if (0 == y)
+            return true;
+        if (1 == y)
+        {
+            if (x != 0 && z != 0)
+                return true;
+        }
+        return false;
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    public int Block(int x, int y, int z)
+    {
+
+        if (x >= worldX || x < 0 || y >= worldY || y < 0 || z >= worldZ || z < 0)
+        {
+            return (byte)1;
+        }
+
+        return vState[x, y, z];
+    }
+
+    public void GenColumn(int x, int z)
+    {
+        for (int y = 0; y < chunks.GetLength(1); y++)
+        {
+            //Create a temporary Gameobject for the new chunk instead of using chunks[x,y,z]
+            GameObject newChunk = Instantiate(chunk, new Vector3(x * chunkSize - 0.5f,
+                y * chunkSize + 0.5f, z * chunkSize - 0.5f), new Quaternion(0, 0, 0, 0)) as GameObject;
+
+            //Now instead of using a temporary variable for the script assign it
+            //to chunks[x,y,z] and use it instead of the old \"newChunkScript\" 
+            chunks[x, y, z] = newChunk.GetComponent("Chunk") as Chunk;
+            chunks[x, y, z].worldReference = gameObject;
+            chunks[x, y, z].chunkSize = chunkSize;
+            chunks[x, y, z].chunkX = x * chunkSize;
+            chunks[x, y, z].chunkY = y * chunkSize;
+            chunks[x, y, z].chunkZ = z * chunkSize;
+        }
+    }
+
+    public void UnloadColumn(int x, int z)
+    {
+        for (int y = 0; y < chunks.GetLength(1); y++)
+        {
+            //Object.Destroy(chunks[x, y, z].gameObject);
+        }
+    }
+}
