8869ba2597d3b88e9957c4f149fd5436f0fdf1f3
Sanlador <cunardr@oregonstate.edu>
2018-03-13 21:03:38 -0700

    Began demo build for ray tracer in VR

---
+++ DemoBuilds/RayTraceVRDemo/Assets/Scripts/Block.cs
@@ -0,0 +1,124 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Block : MonoBehaviour
+{
+
+    public Material RayTracer;
+    public RenderTexture ColorData;
+    public RenderTexture SurfaceData;
+
+
+    public Mesh Model;
+    public const int width = 32;
+    const int layers = 1;
+    const bool doesMipMapping = true;
+    const int groupWidth = 8;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ColorData = null;
+        SurfaceData = null;
+        RayTracer = GetComponent<MeshRenderer>().material;
+        Model = new Mesh();
+        MeshFilter filter = GetComponent<MeshFilter>();
+
+        new Mesh();
+        List<Vector3> Pos = new List<Vector3>();
+        List<Color> Col = new List<Color>();
+
+        for (int i = 0; i < 8; i++)
+        {
+            Pos.Add(new Vector3(i % 2, (i % 4) / 2, i / 4));
+            Col.Add(new Color(i % 2, (i % 4) / 2, i / 4, 1.0f));
+        }
+
+        List<int> Ind = new List<int>
+        {
+            0,3,1,      0,2,3,
+            4,5,7,      4,7,6,
+
+            0,1,5,      0,5,4,
+            2,7,3,      2,6,7,
+
+            0,6,2,      0,4,6,
+            1,3,7,      1,7,5
+        };
+
+
+        Model.SetVertices(Pos);
+        Model.SetColors(Col);
+        Model.SetIndices(Ind.ToArray(), MeshTopology.Triangles, 0);
+        filter.mesh = Model;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    RenderTexture makeDataGrid(int width, int height, int depth)
+    {
+        RenderTexture result = new RenderTexture(width, height, 0);
+        result.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+        result.width = width;
+        result.height = height;
+        result.volumeDepth = depth;
+        result.enableRandomWrite = true;
+        result.filterMode = FilterMode.Point;
+        result.format = RenderTextureFormat.ARGBFloat;
+        result.autoGenerateMips = false;
+        result.useMipMap = false;
+        result.Create();
+        return result;
+    }
+
+    RenderTexture makeSeekGrid(int width, int height, int depth)
+    {
+        RenderTexture result = new RenderTexture(width, height, 0);
+        result.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+        result.width = width;
+        result.height = height;
+        result.volumeDepth = depth;
+        result.enableRandomWrite = true;
+        result.filterMode = FilterMode.Point;
+        result.format = RenderTextureFormat.R8;
+        result.autoGenerateMips = false;
+        result.useMipMap = false;
+        result.Create();
+        return result;
+    }
+
+
+    public void BrushOperation(ComputeShader brushShader, int kernelIndex)
+    {
+        if (ColorData == null)
+        {
+            ColorData = makeDataGrid(width, width, width);
+            RayTracer.SetTexture("ColorData", ColorData);
+
+            SurfaceData = makeDataGrid(width, width, width);
+            RayTracer.SetTexture("SurfaceData", SurfaceData);
+
+            RayTracer.SetFloat("texWidth", width);
+        }
+        brushShader.SetTexture(kernelIndex, "ColorData", ColorData);
+        brushShader.SetTexture(kernelIndex, "SurfaceData", SurfaceData);
+        brushShader.Dispatch(kernelIndex, width / groupWidth, width / groupWidth, width / groupWidth);
+    }
+    
+
+
+}
+
---
+++ DemoBuilds/RayTraceVRDemo/Assets/Scripts/Brush.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Brush : MonoBehaviour
+{
+
+    Matrix4x4 lastTransform;
+    public ComputeShader CurrentBrush;
+    public ComputeShader WipeBrush;
+
+    public ComputeShader BasicBrush;
+    public List<Block> blocks;
+    // public Color brushColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        lastTransform = transform.worldToLocalMatrix;
+        BasicBrush.SetInt("texWidth", Block.width);
+        setVector4(BasicBrush,"color0",new Vector4(0, 0, 0, 0));
+        setVector4(BasicBrush, "color1",new Vector4(0, 0, 0, 0));
+        setVector4(BasicBrush, "sliders", new Vector4(0, 0, 0, 0));
+        transform.SetPositionAndRotation(new Vector3(0, 0, 0), new Quaternion());
+        transform.localScale = new Vector3(0, 0, 0);
+        Wipe(new Vector4(0,0,0,0),new Vector4(0,0,0,0));
+    }
+
+
+    void setVector4(ComputeShader brush, string name, Vector4 value)
+    {
+        float[] val = new float[4];
+        val[0] = value.x;
+        val[1] = value.y;
+        val[2] = value.z;
+        val[3] = value.w;
+        brush.SetFloats(name, val);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void Down(ComputeShader brush)
+    {
+        if(brush != null)
+        {
+            CurrentBrush = brush;
+        }
+        else
+        {
+            CurrentBrush = BasicBrush;
+        }
+        lastTransform = transform.worldToLocalMatrix;
+    }
+
+    public void Stroke(Vector4 color0, Vector4 color1, Vector4 sliders)
+    {
+        if(CurrentBrush != null)
+        {
+            PerformBrush(CurrentBrush, color0, color1, sliders, lastTransform, transform.worldToLocalMatrix);
+        }
+        lastTransform = transform.worldToLocalMatrix;
+    }
+
+    public void Up()
+    {
+        CurrentBrush = null;
+    }
+
+
+
+    public void PerformBrush(  ComputeShader brush, Vector4 color0, Vector4 color1, Vector4 sliders,
+                        Matrix4x4 startTransform, Matrix4x4 endTransform)
+    {
+        int kInd = BasicBrush.FindKernel("main");
+        float[] offset = new float[3];
+
+        setVector4(brush,"color0", color0);
+        setVector4(brush, "color1", color1);
+        setVector4(brush, "sliders", sliders);
+
+        brush.SetMatrix("start", startTransform);
+        brush.SetMatrix("end", endTransform);
+
+        foreach (Block b in blocks)
+        {
+            offset[0] = b.transform.position.x;
+            offset[1] = b.transform.position.y;
+            offset[2] = b.transform.position.z;
+            BasicBrush.SetFloats("baseOffset", offset);
+            b.BrushOperation(BasicBrush, kInd);
+        }
+    }
+
+
+    public void Wipe( Vector4 color0, Vector4 color1 )
+    {
+        PerformBrush(WipeBrush, color0, color1, new Vector4(0, 0, 0), new Matrix4x4(), new Matrix4x4());        
+    }
+
+
+}
